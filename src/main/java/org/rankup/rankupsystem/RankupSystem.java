c75f341e57259ea9cbf7e2aa9749a36aa5b76cc6
Frags <adriwen600@gmail.com>
2024-03-06 21:49:00 +0000

    test

--- src/main/java/org/rankup/rankupsystem/RankupSystem.java
+++ src/main/java/org/rankup/rankupsystem/RankupSystem.java
@@ -1,17 +1,37 @@
 package org.rankup.rankupsystem;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.rankup.rankupsystem.files.DataManager;
 
-public final class RankupSystem extends JavaPlugin {
+//setconfig
+//saveConfig()
+public final class RankupSystem extends JavaPlugin implements Listener {
+
+    public DataManager data;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        System.out.println("test");
+        this.data = new DataManager(this);
+        getServer().getPluginManager().registerEvents(this,this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        Player player = e.getPlayer();
+
+        data.getConfig().set(player.getDisplayName(), 1);
+        data.saveConfig();
+    }
+
+
 }

ec732608debb7ae485eb2735fb8ef30f5626112b
Frags <adriwen600@gmail.com>
2024-03-06 21:16:22 +0000

    test

--- src/main/java/org/rankup/rankupsystem/RankupSystem.java
+++ src/main/java/org/rankup/rankupsystem/RankupSystem.java
@@ -7,7 +7,7 @@ public final class RankupSystem extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        System.out.println("test");
     }
 
     @Override
